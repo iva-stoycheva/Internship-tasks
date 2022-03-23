62ea0d431d2b3dcfdace01d184176e5c5eecdceb
Ива <ivastoycheva@abv.bg>
2022-03-23 15:04:21 +0200

    Changes Task3

--- Task3/src/main/java/read/ReadFile.java
+++ Task3/src/main/java/read/ReadFile.java
@@ -8,9 +8,10 @@ import java.io.IOException;
 public class ReadFile {
     public void read(String inputFile) throws IOException {
         File file = new File(inputFile);
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-        String line;
-        while ((line = bufferedReader.readLine()) != null)
-            System.out.println(line);
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+                System.out.println(line);
+        }
     }
 }
--- Task3/src/main/java/write/WriteToFile.java
+++ Task3/src/main/java/write/WriteToFile.java
@@ -1,17 +1,48 @@
 package write;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class WriteToFile {
+
+    public static final String OUTPUT_FILE_FORMAT = "%d %s %d = %.2f\n";
+
     public void write(String inputFile) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(inputFile));
+        File file = new File(inputFile);
+
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            for (String line : lines) {
+                String[] splitElements = line.split(" ");
+                int firstNum = Integer.parseInt(splitElements[0]);
+                String operation = splitElements[1];
+                int secondNum = Integer.parseInt(splitElements[2]);
+
+                float result;
+                switch (operation) {
+                    case "+":
+                        result = firstNum + secondNum;
+                        break;
+                    case "-":
+                        result = firstNum - secondNum;
+                        break;
+                    case "*":
+                        result = firstNum * secondNum;
+                        break;
+                    case "/":
+                        result = (float) firstNum / secondNum;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown operation");
+                }
+                fileWriter.append(String.format(OUTPUT_FILE_FORMAT, firstNum, operation, secondNum, result));
+            }
+        }
+    }
+}
+    /*public void write(String inputFile) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get(inputFile));
         File file=new File(inputFile);
         String newLine=null;
@@ -29,6 +60,4 @@ public class WriteToFile {
                 }
                 fileWriter.append(finalResult + "\n");
             }
-        }
-    }
-}
+        }*/
