9baaf7d06422ccd97430e10bc39d0e65756548a7
iva-stoycheva <ivastoycheva@abv.bg>
2022-05-19 13:47:14 +0300

    Initial commit

--- Task9/src/main/java/Calculator.java
+++ Task9/src/main/java/Calculator.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        /* for testing :
-        binary: (b.10+b.11)*b.100-b.10=?
-        octal: (o.2+o.3)*o.4-o.2=?
-        decimal: (2+3)*4-2=?
-        hexadecimal: (h.2+h.3)*h.4-h.2=?
-        */
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Еnter math expression in binary, octal, decimal or hexadecimal: ");
+            String str = scanner.nextLine();
+            String line = str.substring(0, str.indexOf("="));
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Еnter math expression in binary, octal, decimal or hexadecimal: ");
-        String str = scanner.next();
-        String line = str.substring(0, str.indexOf("="));
-        double result = MathExpression_exp4j.evaluate(line);
-
-        MathExpression_mXparser.evaluate(line);
-        System.out.println();
-
-        /*System.out.println(DecimalToOtherNumberSystem.toBinary(result));
-        System.out.println(DecimalToOtherNumberSystem.toOctal(result));
-        System.out.println(DecimalToOtherNumberSystem.toDecimal(result));
-        System.out.println(DecimalToOtherNumberSystem.toHexadecimal(result));*/
+            String calculationEngine = System.getenv("calculationEngine");
+            if (calculationEngine.equals("rpn")) {
+                String[] strArray = InfixToPostfix.convert(line).split("\\s+");
+                int result = RPN.evaluateRPN(strArray);
+                System.out.println(result);
+            }
+            else if (calculationEngine.equals("mXparser")){
+                MathExpression_mXparser.evaluate(line);
+            }
+            else {
+                System.out.println("Unknown calculation engine: " + calculationEngine);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
