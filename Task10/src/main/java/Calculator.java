f6fd0a5c25dff2128a7e9dfc6ead51c1f7fafa71
iva-stoycheva <ivastoycheva@abv.bg>
2022-06-08 10:58:45 +0300

    Change to H2 and separate results from M/M+/M- into 3 different methods

--- Task10/src/main/java/Calculator.java
+++ Task10/src/main/java/Calculator.java
@@ -1,21 +1,29 @@
 public class Calculator {
-
     public static void main(String[] args) {
-        try {
-            MemoryService memoryService = new MemoryService();
+       MemoryService memoryService = new MemoryService();
+       try {
+          String mathExp1 = memoryService.input();
+          String rightSide1 = mathExp1.substring(mathExp1.indexOf("="));
+          if (rightSide1.contains("M")) {
+             memoryService.insertIntoM(mathExp1);
+          }
 
-            String mathExp1 = memoryService.input();
-            memoryService.insertIntoM(mathExp1);
+          String mathExp2 = memoryService.input();
+          String leftSide2 = mathExp2.substring(0, mathExp2.indexOf("="));
+          String rightSide2 = mathExp2.substring(mathExp2.indexOf("="));
+          if (leftSide2.contains("M") && rightSide2.contains("M+")) {
+             memoryService.insertResultFromCalculationsMplus(mathExp2);
+          }
 
-            String mathExp2 = memoryService.input();
-            memoryService.insertResultFromCalculations(mathExp2);
+          String mathExp3 = memoryService.input();
+          String rightSide3 = mathExp3.substring(mathExp3.indexOf("="));
+          if (rightSide3.contains("M-")) {
+             memoryService.insertResultFromCalculationsMminus(mathExp3);
+          }
 
-            String mathExp3 = memoryService.input();
-            memoryService.insertResultFromCalculations(mathExp3);
-
-            memoryService.selectMemoryResult();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+          memoryService.selectMemoryResult();
+       } catch (Exception e) {
+          e.printStackTrace();
+       }
     }
 }
--- Task10/src/main/java/MemoryService.java
+++ Task10/src/main/java/MemoryService.java
@@ -3,7 +3,6 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 
 import java.sql.*;
 import java.util.Scanner;
-
 public class MemoryService {
     public static final String URL = "jdbc:h2:~/memory";
     public static final String USER = "root";
@@ -35,10 +34,12 @@ public class MemoryService {
         connection.close();
     }
 
-    public void insertResultFromCalculations(String mathExp) throws SQLException, ClassNotFoundException {
+    public void insertResultFromCalculationsMplus(String mathExp) throws SQLException, ClassNotFoundException {
         Class.forName("org.h2.Driver");
         Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        if (mathExp.substring(0, mathExp.indexOf("=")).contains("M")) {
+        String leftSide = mathExp.substring(0, mathExp.indexOf("="));
+        String rightSide = mathExp.substring(mathExp.indexOf("="));
+        if (leftSide.contains("M")) {
             String replacement = "";
             String selection = "SELECT M FROM memory";
             Statement st = connection.createStatement();
@@ -46,7 +47,7 @@ public class MemoryService {
             if (rs.next()) {
                 replacement = mathExp.replace("M", rs.getString(1));
             }
-            if (mathExp.substring(mathExp.lastIndexOf("=") + 1).equals("M+")) {
+            if (rightSide.contains("M+")) {
                 String onlyOperatorsAndOperands2 = replacement.substring(0, replacement.indexOf("="));
                 Expression expression2 = new ExpressionBuilder(onlyOperatorsAndOperands2).build();
                 String insertion2 = "INSERT INTO memory(M)" +
@@ -55,18 +56,32 @@ public class MemoryService {
                 prest2.setDouble(1, rs.getDouble(1) + expression2.evaluate());
                 prest2.executeUpdate();
             }
-            else if (mathExp.substring(mathExp.lastIndexOf("=") + 1).equals("M-")) {
-                String onlyOperatorsAndOperands3 = replacement.substring(0, replacement.indexOf("="));
-                Expression expression3 = new ExpressionBuilder(onlyOperatorsAndOperands3).build();
-                String insertion3 = "INSERT INTO memory(M)" +
-                        "VALUES (?)";
-                PreparedStatement prest3 = connection.prepareStatement(insertion3);
+        }
+        connection.close();
+    }
+
+    public void insertResultFromCalculationsMminus(String mathExp) throws SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        String leftSide = mathExp.substring(0, mathExp.indexOf("="));
+        String rightSide = mathExp.substring(mathExp.indexOf("="));
+        if (rightSide.contains("M-")) {
+            String onlyOperatorsAndOperands3 = mathExp.substring(0, mathExp.indexOf("="));
+            Expression expression3 = new ExpressionBuilder(onlyOperatorsAndOperands3).build();
+            String selection = "SELECT M FROM memory ORDER BY id DESC LIMIT 1";
+            String insertion3 = "INSERT INTO memory(M)" +
+                    "VALUES (?)";
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(selection);
+            PreparedStatement prest3 = connection.prepareStatement(insertion3);
+            while (rs.next()) {
                 prest3.setDouble(1, rs.getDouble(1) - expression3.evaluate());
                 prest3.executeUpdate();
             }
         }
         connection.close();
     }
+
 
     public void selectMemoryResult() throws SQLException, ClassNotFoundException{
         double result;
